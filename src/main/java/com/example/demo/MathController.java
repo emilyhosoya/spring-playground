86370171b579873054960694e1cdf8ad1a44290a
Emily Hosoya <emilyhosoya@gmail.com>
2020-12-08 13:57:01 -0700

    getCalculation works

--- src/main/java/com/example/demo/MathController.java
+++ src/main/java/com/example/demo/MathController.java
@@ -1,9 +1,6 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/math")
@@ -13,5 +10,31 @@ public class MathController {
     public double getPi() {
         return Math.PI;
     }
+
+    @GetMapping("/calculate")
+    public String getCalculation(@RequestParam( defaultValue = "add" ) String operation, @RequestParam int x, @RequestParam int y) {
+        String operand = null;
+        int result = 0;
+        switch (operation) {
+            case "add":
+                operand = " + ";
+                result = x + y;
+                break;
+            case "subtract":
+                operand = " - ";
+                result = x - y;
+                break;
+            case "multiply":
+                operand = " * ";
+                result = x * y;
+                break;
+            case "divide":
+                operand = " / ";
+                result = x / y;
+                break;
+        }
+        return x + operand + y + " = " + result;
+    }
+    
 
 }
--- src/test/java/com/example/demo/MathControllerTest.java
+++ src/test/java/com/example/demo/MathControllerTest.java
@@ -25,4 +25,49 @@ public class MathControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string(Double.toString(Math.PI)));
     }
+
+    @Test
+    public void testAdd() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/math/calculate?operation=add&x=4&y=6");
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 + 6 = 10"));
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/math/calculate?operation=multiply&x=4&y=6");
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 * 6 = 24"));
+    }
+
+    @Test
+    public void testSubtract() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/math/calculate?operation=subtract&x=4&y=6");
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("4 - 6 = -2"));
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/math/calculate?operation=divide&x=30&y=5");
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("30 / 5 = 6"));
+    }
+
+    @Test
+    public void testNoOperand() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/math/calculate?x=30&y=5");
+
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("30 + 5 = 35"));
+    }
 }
