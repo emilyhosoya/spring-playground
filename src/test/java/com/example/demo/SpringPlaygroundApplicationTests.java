6acfe9a8478188fb74ee85939eb75edd0c1d91e1
Emily Hosoya <emilyhosoya@gmail.com>
2020-12-14 13:36:10 -0700

    Got one test to work and pass

--- src/test/java/com/example/demo/SpringPlaygroundApplicationTests.java
+++ src/test/java/com/example/demo/SpringPlaygroundApplicationTests.java
@@ -2,12 +2,22 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 @SpringBootTest
 class SpringPlaygroundApplicationTests {
+
+	@Autowired
+	WordCounter wordCounter;
+
+	@Test
+	public void testWordCounter() {
+		assertThat(wordCounter.getWords(), equalTo(""));
+	}
 
 	@Test
 	void contextLoads() {
