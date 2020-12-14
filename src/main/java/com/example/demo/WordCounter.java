c0e297e7c7c229ca564b8a352d7d55106f387658
Emily Hosoya <emilyhosoya@gmail.com>
2020-12-14 16:20:07 -0700

    case sensitivity works, but not skip words

--- src/main/java/com/example/demo/WordCounter.java
+++ src/main/java/com/example/demo/WordCounter.java
@@ -3,12 +3,21 @@ package com.example.demo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Configuration
+@Service
 public class WordCounter {
+    private MyConfig config;
+
+    public WordCounter(MyConfig config) {
+        this.config = config;
+    }
+
     private String words;
 
     @Bean
@@ -22,13 +31,20 @@ public class WordCounter {
         this.words = words;
         String[] array = words.split(" ");
         Map<String, Integer> map = new HashMap<>();
-        for (String element : array) {
-            String word = element.toLowerCase();
-            if (map.containsKey(word)) {
-                int count = map.get(word);
-                map.put(word, count + 1);
-            } else {
-                map.put(word, 1);
+        for (String word : array) {
+            if (!config.getCaseSensitive()) {
+                word = word.toLowerCase();
+            }
+            // not sure why this isn't working!
+            for (String skips : config.getWords().getSkip()) {
+                if (!word.equals(skips)) {
+                    if (map.containsKey(word)) {
+                        int count = map.get(word);
+                        map.put(word, count + 1);
+                    } else {
+                        map.put(word, 1);
+                    }
+                }
             }
         }
         return map;
--- src/main/java/com/example/demo/WordCounterController.java
+++ src/main/java/com/example/demo/WordCounterController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,16 +14,18 @@ import java.io.StringWriter;
 @RestController
 @RequestMapping("/words")
 public class WordCounterController {
-
+    @Autowired
+    private MyConfig config;
     private Map<String, Integer> map;
 
-    public WordCounterController(Map map) {
+    public WordCounterController(Map map, MyConfig config) {
         this.map = map;
+        this.config = config;
     }
 
     @PostMapping("/count")
     public String countWords(@RequestBody String words) throws IOException {
-        WordCounter wordCounter = new WordCounter();
+        WordCounter wordCounter = new WordCounter(config);
         Map<String, Integer> map = wordCounter.count(words);
         StringWriter out = new StringWriter();
         JSONValue.writeJSONString(map, out);
